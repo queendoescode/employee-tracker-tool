b8294d5c4d98d4656e05f2206ea1d78bf7498eaf
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-18 00:02:44 -0400

    implement first task.

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection(
-  {
-    host: 'localhost', // the local machine
-    user: 'root', // database login - default user name when you install mysql
-    password: '', // empty password is the default, otherwise would be actual password
-    database: 'employee_tracker_db' // make this the current database for the connection
-  }
-);
+    {
+      host: 'localhost', // the local machine
+      user: 'root', // database login - default user name when you install mysql
+      password: '', // empty password is the default, otherwise would be actual password
+      database: 'employee_tracker_db' // make this the current database for the connection
+    }
+  );
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 
-/* Test the connection
-
-db.query(
-  'SELECT * FROM department', 
-  (err, results) => {
-    console.log(results);
-  });
-*/
-
-
-inquirer
+function taskMenu() {
+  inquirer
   .prompt([
     {
       name:"task", 
@@ -31,15 +22,49 @@ inquirer
   .then(answers => {
     console.log(answers);
 
+    switch (answers.task) {
+      case "view all departments":
+        db.query(
+          'SELECT * FROM department', 
+          (err, results) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(results);
+            }
+          });
+        break;
+      case "view all roles":
+        break;
+      case "view all employees":
+        break;
+      case "add a department":
+        break;
+      case  "add a role":
+        break;
+      case "add an employee":
+        break; 
+      case "update an employee role":
+        break;
+    }
+
   })
   .catch(err => {
     console.error(err);
     console.error('There was a problem asking questions. Ask the developer to debug.');
-  });
+  })
+  .finally(
+    () => db.end( err => {
+      if(err) {
+        console.error("Error closing connection to database");
+      }
+    } )
+  );
+}
+
+
+taskMenu();
+      
+
   
 
-  db.end(
-    err => {
-      console.error("Error closing connection to database");
-    }
-  );
