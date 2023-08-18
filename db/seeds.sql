2bb81fa927b926affbe8ab0be8faae7872d42d6b
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-17 23:20:03 -0400

    added module for database connection and index.js for main program.

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,3 +18,16 @@ insert into employee (first_name, last_name, role_id, manager_id)
   values('Sally','Smith',3,1); 
 insert into employee (first_name, last_name, role_id, manager_id) 
   values('Fred','Law',1,1);
+
+/*
+mysql> select employee.id,first_name,last_name,role.title,role.salary,department.name as department 
+from employee join role on role.id=role_id join department on department.id=department_id;
++----+------------+-----------+-------------------+--------+------------+
+| id | first_name | last_name | title             | salary | department |
++----+------------+-----------+-------------------+--------+------------+
+|  1 | Owen       | Jones     | CEO               | 100000 | Executive  |
+|  2 | Sally      | Smith     | Senior Accountant |  75000 | Finance    |
+|  3 | Fred       | Law       | Sales Manager     |  50000 | Sales      |
++----+------------+-----------+-------------------+--------+------------+
+3 rows in set (0.00 sec)
+*/
---
+++ index.js
@@ -0,0 +1,10 @@
+const db = require('./db/connection');
+
+/* Test the connection
+
+db.query(
+  'SELECT * FROM department', 
+  (err, results) => {
+    console.log(results);
+  });
+*/
