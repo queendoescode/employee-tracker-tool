9eec44864f74f355a1d227e2792007fdf249dc9d
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-17 23:41:15 -0400

    added first question.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db = require('./db/connection');
+const inquirer = require('inquirer');
 
 /* Test the connection
 
@@ -8,3 +9,37 @@ db.query(
     console.log(results);
   });
 */
+
+
+inquirer
+  .prompt([
+    {
+      name:"task", 
+      message:"What would you like to do?",
+      type: "list",
+      choices: [
+        "view all departments", 
+        "view all roles", 
+        "view all employees", 
+        "add a department", 
+        "add a role", 
+        "add an employee", 
+        "update an employee role"
+      ]
+    }
+  ])
+  .then(answers => {
+    console.log(answers);
+
+  })
+  .catch(err => {
+    console.error(err);
+    console.error('There was a problem asking questions. Ask the developer to debug.');
+  });
+  
+
+  db.end(
+    err => {
+      console.error("Error closing connection to database");
+    }
+  );
