cf3c1194c030cb5000f630e488a1afd79fb9de90
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-19 20:34:29 -0400

    change to Promise interface for mysql2.

--- index.js
+++ index.js
@@ -1,70 +1,73 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
-
-function taskMenu() {
-  inquirer
-  .prompt([
-    {
-      name:"task", 
-      message:"What would you like to do?",
-      type: "list",
-      choices: [
-        "view all departments", 
-        "view all roles", 
-        "view all employees", 
-        "add a department", 
-        "add a role", 
-        "add an employee", 
-        "update an employee role"
-      ]
-    }
-  ])
-  .then(answers => {
-    console.log(answers);
-
-    switch (answers.task) {
-      case "view all departments":
-        db.query(
-          'SELECT * FROM department', 
-          (err, results) => {
-            if (err) {
-              console.error(err);
-            } else {
-              console.log(results);
-            }
-          });
-        break;
-      case "view all roles":
-        break;
-      case "view all employees":
-        break;
-      case "add a department":
-        break;
-      case  "add a role":
-        break;
-      case "add an employee":
-        break; 
-      case "update an employee role":
-        break;
-    }
-
-  })
-  .catch(err => {
-    console.error(err);
-    console.error('There was a problem asking questions. Ask the developer to debug.');
-  })
-  .finally(
-    () => db.end( err => {
-      if(err) {
-        console.error("Error closing connection to database");
-      }
-    } )
-  );
-}
+const Queries = require('./db/queries');
 
 
-taskMenu();
-      
-
+db.then( connection => {
+  const dataAccess = new Queries(connection);
   
+  inquirer
+    .prompt([
+      {
+        name:"task", 
+        message:"What would you like to do?",
+        type: "list",
+        choices: [
+          "view all departments", 
+          "view all roles", 
+          "view all employees", 
+          "add a department", 
+          "add a role", 
+          "add an employee", 
+          "update an employee role"
+        ]
+      }
+    ])
+    .then(answers => {
+      console.log(answers);
+  
+      switch (answers.task) {
+        case "view all departments":
+          // When using the Promise version of mysql2, results is an array of two items
+          // The returned rows is first item, and table metadata is the 2nd row. 
+          // We only need to look at the returned rows.
 
+          dataAccess.getDepartments()
+            .then(results => {
+              console.log(results[0]);
+            });
+          break;
+        case "view all roles":
+          dataAccess.getRoles()
+            .then(results => console.log(results[0]));
+          break;
+        case "view all employees":
+          break;
+        case "add a department":
+          break;
+        case "add a role":
+          break;
+        case "add an employee":
+          break; 
+        case "update an employee role":
+          break;
+      }
+  
+    })
+    .catch(err => {
+      console.error(err);
+      console.error('There was a problem asking questions. Ask the developer to debug.');
+    })
+    .finally(
+      () => connection.end( err => {
+        if(err) {
+          console.error("Error closing connection to database");
+        }
+      } )
+    );
+  }
+);
+
+
+
+ 
